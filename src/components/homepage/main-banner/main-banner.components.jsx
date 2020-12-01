50858372a982769e5eba288bbff34716c40a4f88
Suranjan Rai <Suranjanthulungrai@gmail.com>
2020-12-02 01:05:46 +0545

    banner only

--- src/components/homepage/main-banner/main-banner.components.jsx
+++ src/components/homepage/main-banner/main-banner.components.jsx
@@ -23,24 +23,26 @@ const MainBanner = () => {
       className="background"
       fluid={data.backgroundImage.childImageSharp.fluid}
     >
-      <div className="row background__details">
-        <div className="col-md-8 background__details-main">
-          <h1>
-            Education Courses. <span>Study For Better Future</span>
-          </h1>
-          <p>
-            Onec Consequat Sapien Ut Leo Cursus Rhoncus. Nullam Dui Mi,
-            Vulputate Ac Metus Semper Nullam Dui Mi. Vestibulum Ante. Morbi At
-            Dui Nisl.
-          </p>
-          <Link to="/about" className="background__details-main-link">
-            Read More
-          </Link>
-        </div>
-        <div className="col-md-4 background__details-secondary">
-          <h3>Find a Courses and Join Us </h3>
-          <ContactForm />
-          <h4>We will get back at you within 24hrs.</h4>
+      <div className="container-fluid">
+        <div className="row background__details">
+          <div className="col-md-8 background__details-main">
+            <h1>
+              Education Courses. <span>Study For Better Future</span>
+            </h1>
+            <p>
+              Onec Consequat Sapien Ut Leo Cursus Rhoncus. Nullam Dui Mi,
+              Vulputate Ac Metus Semper Nullam Dui Mi. Vestibulum Ante. Morbi At
+              Dui Nisl.
+            </p>
+            <Link to="/about" className="background__details-main-link">
+              Read More
+            </Link>
+          </div>
+          <div className="col-md-4 background__details-secondary">
+            <h3>Find a Courses and Join Us </h3>
+            <ContactForm />
+            <h4>We will get back at you within 24hrs.</h4>
+          </div>
         </div>
       </div>
     </BackgroundImage>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import SEO from "./../components/SEO"
 import Layouts from "../components/layouts/layouts.components"
-import VideoBanner from "../components/homepage/video-banner/video-banner.components"
+
 const About = () => {
   return (
     <Layouts>
       <SEO title="About" />
-      <VideoBanner />
+      hello from about us
     </Layouts>
   )
 }
